d28bf8d8eb1c4a03bfe27bd393fa2bdc8ba4028e
davidpovarsky <davidpovarski1@gmail.com>
2025-12-07 01:43:02 +0200

    Fix formatting and complete isoDateTodayLocal function

--- utils.js
+++ utils.js
@@ -14,5 +14,7 @@ function isoDateTodayLocal() {
   const year = now.getFullYear();
   const month = String(now.getMonth() + 1).padStart(2, "0");
   const day = String(now.getDate()).padStart(2, "0");
-  return `${year}
+return `${year}-${month}-${day}`;
+}
+
 
